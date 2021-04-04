03e73aac583d869fc6c51182a82caccbf4fa267e
Aslam Ali <rks507469@gmail.com>
2021-04-04 19:42:17 +0530

    added canvas

--- JS/index.js
+++ JS/index.js
@@ -1,3 +1,8 @@
+// background canvas
+let bgCanvas = document.getElementById("canvasBg"),
+    ctxBg    = bgCanvas.getContext("2d");
+
+// some code
 var red_tank=document.getElementById("red_tank");
 console.log(red_tank)
 red_tank.style="rotate(20deg)";
