1f02ff2914c933c5db609013887c12b032f67d4e
Aslam Ali <rks507469@gmail.com>
2021-04-05 12:22:27 +0530

    doing some changes

--- JS/index.js
+++ JS/index.js
@@ -1,8 +1,13 @@
 // background canvas
-let bgCanvas = document.getElementById("canvasBg"),
-    ctxBg    = bgCanvas.getContext("2d");
+let bgCanvas     = document.getElementById("bgCanvas"),
+    ctxBg        = bgCanvas.getContext("2d");
+    canvasWidth  = bgCanvas.width;
+    canvasHeight = bgCanvas.height; 
+
+let bgImg = "./Assets/Grass.png";
+    ctxBg.drawImage(bgImg, canvasWidth, canvasHeight);
 
 // some code
-var red_tank=document.getElementById("red_tank");
-console.log(red_tank)
-red_tank.style="rotate(20deg)";
+// var red_tank=document.getElementById("red_tank");
+// console.log(red_tank)
+// red_tank.style="rotate(20deg)";
