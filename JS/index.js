6d3d290a1767ad89210db83283b31c27781d7454
Aslam Ali <rks507469@gmail.com>
2021-04-05 13:11:54 +0530

    finally some images!

--- JS/index.js
+++ JS/index.js
@@ -4,8 +4,12 @@ let bgCanvas     = document.getElementById("bgCanvas"),
     canvasWidth  = bgCanvas.width;
     canvasHeight = bgCanvas.height; 
 
-let bgImg = "./Assets/Grass.png";
-    ctxBg.drawImage(bgImg, canvasWidth, canvasHeight);
+let bgImg = new Image();
+    bgImg.onload = function() {
+        ctxBg.drawImage(bgImg, 0, 0);
+    }
+    bgImg.src = 'Assets/Sand.png';
+//console.log(typeof(bgImg));
 
 // some code
 // var red_tank=document.getElementById("red_tank");
